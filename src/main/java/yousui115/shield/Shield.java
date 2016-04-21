804e5cf2499e0d4d2028b6298c83f99d05ae7139
yousui115 <yousui115@gmail.com>
2016-04-21 23:45:37 +0900

    リービージョーンー！
    いつも忘れて嫌になる

--- src/main/java/yousui115/shield/Shield.java
+++ src/main/java/yousui115/shield/Shield.java
@@ -43,7 +43,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class Shield
 {
     public static final String MODID = "shield";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "v2";
 
     public static Enchantment enchGuard;
 
