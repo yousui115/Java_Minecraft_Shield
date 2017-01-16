c42ac1ab5d32415bf40b28550d786af13a006587
yousui115 <yousui115@gmail.com>
2017-01-17 00:43:18 +0900

    MC1102 v3
    パワーバッシュをヒットさせた際、対象の数x2の耐久値が減っていた不具合を修正

--- src/main/java/yousui115/shield/network/MsgBashHdl.java
+++ src/main/java/yousui115/shield/network/MsgBashHdl.java
@@ -31,39 +31,10 @@ public class MsgBashHdl implements IMessageHandler<MsgBash, IMessage>
 
             //■メインターゲットをノックバック＋ダメージ(バッシュ強度依存)
             target.knockBack(player, 0.5F * (float)power, player.posX - target.posX, player.posZ - target.posZ);
-            target.attackEntityFrom(DamageSource.causePlayerDamage(player), power - msg.getAmount());
+            target.attackEntityFrom(DamageSource.causePlayerDamage(player), msg.getAmount());
 
+            //■ドンムブ
             Util.tameAIDonmov(target, power);
-
-            //■バッシュが決まってるのでドンムブのポーション効果付与
-//            Potion potion = Potion.REGISTRY.getObject(new ResourceLocation("slowness"));
-//            potion.registerPotionAttributeModifier(SharedMonsterAttributes.MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", -1.0D, 2);
-//            target.addPotionEffect(new PotionEffect(potion, 20 * power, 2));
-
-            //▼パワーバッシュの場合
-//            if (power == 2)
-//            {
-//                //■メインターゲット周囲に居るサブターゲットをピックアップ
-//                for (EntityLiving subTarget : player.worldObj.getEntitiesWithinAABB(EntityLiving.class, target.getEntityBoundingBox().expand(1.0D, 0.25D, 1.0D)))
-//                {
-//                    //▼メインターゲット以外 かつ プレイヤーと同チーム以外 かつ 距離が9より近い
-//                    if (subTarget != target
-//                        && !player.isOnSameTeam(subTarget)
-//                        && player.getDistanceSqToEntity(subTarget) < 9.0D)
-//                    {
-//                        //■サブターゲットをノックバック＋ダメージ(バッシュ強度依存)
-//                        subTarget.knockBack(player, 0.5F * (float)power, (double)MathHelper.sin(player.rotationYaw * 0.017453292F), (double)(-MathHelper.cos(player.rotationYaw * 0.017453292F)));
-//                        subTarget.attackEntityFrom(DamageSource.causePlayerDamage(player), message.getAmount());
-//
-//                        tameAIDonmov(subTarget, power);
-//                        //■バッシュが決まるとドンムブ
-////                        subTarget.addPotionEffect(new PotionEffect(potion, 20 * power, 2));
-//                    }
-//                }
-//
-//                player.worldObj.playSound((EntityPlayer)null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_BLAZE_HURT, player.getSoundCategory(), 1.0F, 1.0F);
-//                player.spawnSweepParticles();
-//            }
 
             //■バッシュ打撃音
             player.worldObj.playSound((EntityPlayer)null, player.posX, player.posY, player.posZ, SoundEvents.ITEM_SHIELD_BLOCK, player.getSoundCategory(), 1.0F, 1.0F);
